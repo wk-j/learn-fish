36724c932bff46d78a8c7b1fb256451d42deabdd
wk-j <somnuk.wk@gmail.com>
2017-12-15 01:35:44 +0700

    Complete tutorial

--- hello.fish
+++ hello.fish
@@ -56,3 +56,73 @@ echo In (pwd), running (uname)
 
 set os (uname)
 echo $os
+
+touch "testing_"(date +%s)".txt"
+ls *.txt
+
+## Combineers (And, Or, Not)
+
+echo abc; and echo efg
+echo abc; or echo  efg
+
+## Conditionals (If, Else, Switch)
+
+if grep fish /etc/shells
+    echo Found fish
+else if grep bash /etc/shells
+    echo Found bash
+else
+    echo Get nothing
+end
+
+switch (uname)
+case Linux
+    echo Hi Tux!
+case Darwin
+    echo Hi Hexley!
+case FreeBSD NetBSD DragonFly
+    echo Hi Beastie!
+case '*'
+    echo Hi, stranger!
+end
+
+## Functions
+function say_hello
+    echo Hello $argv
+end
+
+say_hello
+say_hello everybody!
+
+functions
+
+functions ls
+
+for file in *.txt
+    echo $file
+end
+
+for x in (seq 5)
+    echo $x
+end
+
+## Prompt
+
+function fish_prompt
+    echo "New Prompt % "
+end
+
+function fish_prompt
+    set_color purple
+    date "+%m/%d/%y"
+    set_color FF0
+    echo (pwd) '>'
+    set_color normal
+end
+
+## Startup (Where's .bashrc?)
+cat ~/.config/fish/config.fish
+
+## Autoloading Functions
+
+## Unversal Variables
